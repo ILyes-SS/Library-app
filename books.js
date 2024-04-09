0c75ff90f86ddb3bbf8e5e8edd39e8a4f71ea39f
Senpai-cod <155851163+Senpai-cod@users.noreply.github.com>
2024-04-09 15:48:24 +0100

    made the initial look of the website

---
+++ books.js
@@ -0,0 +1,16 @@
+function Book(title,author,pages,read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function(){
+        return(
+                this.title + " by "+
+                this.author + ", " +
+                this.pages + ", " +
+                this.read
+        );
+    };
+}
+const theHobbit = new book("The Hobbit","J.R.R. Tolkien","295 pages","not read yet");
+console.log(theHobbit.info());
