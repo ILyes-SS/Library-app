e2aecdb41859b4c3a502d1728159d063141d13dd
Senpai-cod <155851163+Senpai-cod@users.noreply.github.com>
2024-04-11 18:22:29 +0100

    Add a dialog box that receive the informations of the book

--- books.js
+++ books.js
@@ -1,9 +1,4 @@
-const theHobbit = new Book("The Hobbit","J.R.R. Tolkien","295 pages","not read yet");
-
-
-const myLibrary = [theHobbit];
-
-
+const myLibrary = [];
 
 function Book(title,author,pages,read){
     this.title = title;
@@ -19,34 +14,57 @@ Book.prototype.info = function(){
             this.read
     );
     }
-
-    console.log(theHobbit.info());
-
-function addBookTOLibrary(){
-    let title = prompt("enter the title of your book");
-    let author = prompt("enter the author name of your book");
-    let pages = prompt("enter the number of pages of your book");
-    let read = prompt("have you read the book");
-
-    let book = new Book(title,author,pages,read);
-        return myLibrary.push(book); 
-    }
-function displayBooks(){
-
-   for(let i=0 ; i < myLibrary.length ; i++){ 
     const cards = document.querySelector(".cards");
+    
+function displayBooks(){
+    cards.innerHTML = "";
+   for(let i=0 ; i < myLibrary.length ; i++){ 
     const card = document.createElement("div");
     card.setAttribute("class","card");
+    
+    const remove = document.createElement("button");
 
     const cardInfos = document.createElement("div");
     cardInfos.innerHTML = `Title: ${myLibrary[i].title} <br>
                             Author: ${myLibrary[i].author} <br>
                              Pages: ${myLibrary[i].pages}<br>
                              reading state:${myLibrary[i].read}`
-
+    cardInfos.appendChild(remove);
     card.appendChild(cardInfos);
     cards.appendChild(card);
    }
 }
 
-displayBooks();
+const button = document.querySelector(".addBook");
+const closeButton = document.querySelector(".cancel");
+const confirmButton = document.querySelector(".confirm");
+
+const dialog = document.querySelector("dialog");
+const body = document.querySelector(".body");
+
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const pagesInput = document.querySelector("#pages");
+const readInput = document.querySelector("#read");
+
+function addBookTOLibrary(){
+    let title = titleInput.value
+    let author = authorInput.value
+    let pages = pagesInput.value
+    let read = readInput.checked ? "read" : "not read";
+
+    let book = new Book(title,author,pages,read);
+        return myLibrary.push(book); 
+    }
+
+button.addEventListener("click", ()=>{
+    dialog.showModal();
+    cards.removeChild(card);
+})
+
+confirmButton.addEventListener("click", (event)=>{
+    event.preventDefault();
+    dialog.close();
+    addBookTOLibrary();
+    displayBooks();
+});
